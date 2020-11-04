af3a9f958d5919484b43b83f79db2629cd3d8e6a
dorianperic <dperic00@fesb.hr>
2020-11-04 11:22:41 +0100

    Labovi samostalan rad

--- postgres-express-node/services/index.js
+++ postgres-express-node/services/index.js
@@ -1,7 +1,8 @@
 const winston = require("winston");
 const UserService = require("./user.service");
 const { User } = require("../models");
-
+const LoginService = require("./login.service");
 const logger = winston.loggers.get("logger");
 
 exports.userServiceInstance = new UserService({ logger, userModel: User });
+exports.loginServiceInstance = new LoginService({ logger, userModel: User });
